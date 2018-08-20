661b3286c9d37f07a8cee48415bf61f749916109
王彦宇 <wangyanyu@wangyanyudeMacBook-Pro.local>
2018-08-20 16:15:06 +0800

    add vendor

--- depot.go
+++ depot.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	cluster := flag.String("cluster", "127.0.0.1:30304", "Comma separated cluster peers")
+	fmt.Println(cluster)
 }
